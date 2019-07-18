63fc67b252531ecb75fcc104f46e99639f55fae6
ivan <ischenko.ivan.ivanovich@gmail.com>
2019-07-18 13:31:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.slider__laptop').slick({
+});
+    $('.close__btn').click(function () {
+        $('.popup').removeClass('open')
+    });
+    $('.open-popup').click(function () {
+        $('.popup').addClass('open')
+    })
+});
