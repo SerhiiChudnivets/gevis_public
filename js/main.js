b946155d846743822d164f240404cc53f8289b6d
ivan <ischenko.ivan.ivanovich@gmail.com>
2019-08-21 10:52:36 +0300

    add animations

--- js/main.js
+++ js/main.js
@@ -15,3 +15,47 @@ $(document).ready(function(){
         $(".mobile__menu").removeClass('show');
     });
 });
+$(document).ready(function(){
+    lazyload();
+});
+$(window).scroll(function(){
+    lazyload();
+})
+function lazyload(){
+    $('[data-counter]').each(function(){
+        if(isScrolledIntoView($(this))){
+            if (!$(this).hasClass('scroll')){
+                counter(this.getAttribute('data-counter'), this);
+            }
+        }
+    });
+}
+function isScrolledIntoView(elem){
+    let $elem = $(elem);
+    let $window = $(window);
+    let docViewTop = $window.scrollTop();
+    let docViewBottom = docViewTop + $window.height();
+    let elemTop = $elem.offset().top;
+    let elemBottom = elemTop + $elem.height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+function counter(data,element) {
+    let start = 0;
+    let finish= data;
+    let timeout;
+    if (data>100){
+        timeout = 10;
+    }
+    else{
+        timeout=60;
+    }
+    $(element).addClass('scroll');
+    let count = setInterval(function (l) {
+        start++;
+        $(element).html(start);
+        console.log(start);
+        if (start==finish){
+            clearInterval(count);
+        }
+    }, timeout)
+}
